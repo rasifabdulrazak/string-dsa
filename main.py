c5e0c0d312eecbc7b4dbb4fdd1735e59e4b9d24c
Rasif <rasifrazak123@gmail.com>
2025-12-27 23:42:44 +0530

    1

--- main.py
+++ main.py
@@ -4,7 +4,22 @@
 # Given a string s consisting of words and spaces, return the length of the last word in the string.
 def count_of_last_word(word):
     length = len(word) - 1
-    pass
+    coun = 0
+    for i in range(length,-1,-1):
+        print(i,"========")
+        if word[i] != " ":
+            print(word[i])
+            coun += 1
+        elif coun > 0:
+            break
+        else:
+            continue
+        print(coun,"=========")
+        
+        
+    return coun
+        
+        
     
     
 print(count_of_last_word("Hello world"))
