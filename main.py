291e573ddb18f9eaca6594aeae36054cc2868dae
Rasif <rasifrazak123@gmail.com>
2025-12-27 21:45:46 +0530

    1

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 # String DSA Questions
+
+
+# Given a string s consisting of words and spaces, return the length of the last word in the string.
+def count_of_last_word(word):
+    length = len(word) - 1
+    pass
+    
+    
+print(count_of_last_word("Hello world"))
