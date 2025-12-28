a9509feea8dabe807916a817501e4c1f7210ad5b
Rasif <rasifrazak123@gmail.com>
2025-12-28 13:21:54 +0530

    2

--- main.py
+++ main.py
@@ -6,20 +6,24 @@ def count_of_last_word(word):
     length = len(word) - 1
     coun = 0
     for i in range(length,-1,-1):
-        print(i,"========")
         if word[i] != " ":
-            print(word[i])
             coun += 1
+            
         elif coun > 0:
             break
         else:
-            continue
-        print(coun,"=========")
-        
+            continue        
         
     return coun
-        
-        
-    
     
 print(count_of_last_word("Hello world"))
+
+# Find Words Containing Character
+def word_contains_index(words:list,x:str):
+    ans = []
+    for i,j in enumerate(words):
+        if x in j:
+            ans.append(i)
+    return ans
+
+print(word_contains_index(["abc","bcs","cab","poi"],"a"))
