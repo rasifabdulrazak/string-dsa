e5bbce25c00b744af7f772e81c51750af2f9ae59
Rasif <rasifrazak123@gmail.com>
2025-12-29 20:12:52 +0530

    2

--- main.py
+++ main.py
@@ -36,6 +36,6 @@ def jwels_and_stones(jewel:str,stones:str):
     for i in stones:
         if i in jewel:
             count+=1
-    return count
+    return count 
 
 print(jwels_and_stones("aA","aAAbbbb"))
